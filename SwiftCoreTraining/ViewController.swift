15a29577275690762b7bac6c773bf545148f6e38
Sergey Lamcev <sergey.lamcev@anoda.mobi>
2019-01-27 18:33:11 +0200

    Removed storyboards

--- SwiftCoreTraining/ViewController.swift
+++ SwiftCoreTraining/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 //        loadImagesConcurently()
-        task1()
+//        task1()
     }
     
     private func loadImagesConcurently() {
