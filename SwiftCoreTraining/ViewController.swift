8a48d31b6323752cac6becbd8462369f86e29ab5
Sergey Lamcev <sergey.lamcev@anoda.mobi>
2019-01-27 17:19:57 +0200

    GCD training

--- SwiftCoreTraining/ViewController.swift
+++ SwiftCoreTraining/ViewController.swift
@@ -9,12 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        loadImagesConcurently()
+        task1()
     }
-
-
+    
+    private func loadImagesConcurently() {
+        let url = "https://picsum.photos/300/300/?image="
+        
+        var imagesURL: [String] = []
+        var images: [UIImage] = []
+        
+        for i in 0..<10 {
+            imagesURL.append(url+"\(i)")
+        }
+        
+        let group = DispatchGroup()
+        
+        for (index, image) in imagesURL.enumerated() {
+            group.enter()
+            DispatchQueue.global().async(group: group){
+                let url = URL(string: image)!
+                URLSession(configuration: .default).dataTask(with: url, completionHandler: { (data, response, error) in
+                    guard
+                        let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                        let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                        let data = data, error == nil else {
+                            print("Failed to load image \(index)")
+                            return
+                    }
+                    if let image = UIImage(data: data) {
+                        print("Downloaded image \(index)")
+                        DispatchQueue.main.async {
+                            images.append(image)
+                        }
+                    }
+                    group.leave()
+                }).resume()
+            }
+        }
+        
+        group.notify(queue: .main) {
+            print("Finished all requests.")
+            print(images.count)
+            print(images[0].size)
+        }
+    }
+    
+    private func task1() {
+        print("A")
+        
+        DispatchQueue.main.async {
+            print("B")
+            
+            DispatchQueue.main.async {
+                print("C")
+                
+                DispatchQueue.main.async {
+                    print("D")
+                    
+                    DispatchQueue.main.sync {
+                        print("E")
+                    }
+                }
+            }
+            
+            DispatchQueue.global().sync {
+                print("F")
+                
+                DispatchQueue.main.sync {
+                    print("G")
+                }
+            }
+            
+            print("H")
+        }
+        
+        print("I")
+    }
+    
+    
 }
 
