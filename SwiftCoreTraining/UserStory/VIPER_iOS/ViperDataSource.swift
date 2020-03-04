e3bf08be78beec9a05a3d8323233f0aa985b9627
Sergey Lamcev <s.lamcev@newlinetechnologies.net>
2020-03-04 20:50:29 +0200

    Viper

---
+++ SwiftCoreTraining/UserStory/VIPER_iOS/ViperDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  ViperDataSource.swift
+//  SwiftCoreTraining
+//
+//  Created by Andrii Mazepa on 04.03.2020.
+//  Copyright © 2020 serglam. All rights reserved.
+//
+
+import Foundation
+
+final class ViperDataSource: DataSource {
+    
+}
